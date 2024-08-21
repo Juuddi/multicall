106dc7809cf0bf38bce6a5d66275a6b32487ed2f
Juuddi <justinrudi26@gmail.com>
2024-08-21 15:44:47 -0500

    Configure for Quai + comment out Uniswap utils

--- src/MultiCall.ts
+++ src/MultiCall.ts
@@ -1,38 +1,36 @@
-import { JsonFragment } from "@ethersproject/abi";
-import { Provider } from "@ethersproject/providers";
-import { Interface } from "ethers/lib/utils";
+import { Interface, JsonFragment, Provider } from 'quais'
 
-import { CallInput, TokenBalances, TokenBalancesAndAllowances, UniswapReservesData } from './types';
-import { toProvider } from "./utils";
-import { multiCall } from './generic';
-import { getBalances, getBalancesAndAllowances } from "./tokens";
-import { getReserves } from "./uniswap";
+import { CallInput, TokenBalances, TokenBalancesAndAllowances } from './types'
+// import { toProvider } from './utils'
+import { multiCall } from './generic'
+import { getBalances, getBalancesAndAllowances } from './tokens'
+// import { getReserves } from "./uniswap";
 
 export class MultiCall {
-  private provider: Provider;
-  constructor(provider: any) {
-    this.provider = toProvider(provider);
-  }
+	private provider: Provider
+	constructor(provider: any) {
+		this.provider = provider
+	}
 
-  public async multiCall(_interface: Interface | JsonFragment[], inputs: CallInput[], strict?: boolean): Promise<[number, any[]]>;
-  public async multiCall(inputs: CallInput[], strict?: boolean): Promise<[number, any[]]>;
-  public async multiCall(arg0: Interface | JsonFragment[] | CallInput[], arg1?: CallInput[] | boolean, arg2?: boolean): Promise<[number, any[]]> {
-    return multiCall(this.provider, arg0, arg1, arg2);
-  }
+	public async multiCall(_interface: Interface | JsonFragment[], inputs: CallInput[], strict?: boolean): Promise<[number, any[]]>
+	public async multiCall(inputs: CallInput[], strict?: boolean): Promise<[number, any[]]>
+	public async multiCall(
+		arg0: Interface | JsonFragment[] | CallInput[],
+		arg1?: CallInput[] | boolean,
+		arg2?: boolean
+	): Promise<[number, any[]]> {
+		return multiCall(this.provider, arg0, arg1, arg2)
+	}
 
-  public async getBalances(tokens: string[], account: string): Promise<[number, TokenBalances]> {
-    return getBalances(this.provider, tokens, account);
-  }
+	public async getBalances(tokens: string[], account: string): Promise<[number, TokenBalances]> {
+		return getBalances(this.provider, tokens, account)
+	}
 
-  public async getBalancesAndAllowances(
-    tokens: string[],
-    owner: string,
-    spender: string
-  ): Promise<[number, TokenBalancesAndAllowances]> {
-    return getBalancesAndAllowances(this.provider, tokens, owner, spender);
-  }
+	public async getBalancesAndAllowances(tokens: string[], owner: string, spender: string): Promise<[number, TokenBalancesAndAllowances]> {
+		return getBalancesAndAllowances(this.provider, tokens, owner, spender)
+	}
 
-  public async getReserves(pairs: string[]): Promise<[number, UniswapReservesData]> {
-    return getReserves(this.provider, pairs);
-  }
+	// public async getReserves(pairs: string[]): Promise<[number, UniswapReservesData]> {
+	//   return getReserves(this.provider, pairs);
+	// }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,22 +1,20 @@
-import { Provider, Web3Provider } from '@ethersproject/providers';
-import { JsonFragment } from "@ethersproject/abi";
-import { Interface } from "ethers/lib/utils";
+import { Interface, JsonFragment, Provider, BrowserProvider } from 'quais'
 
-export function toProvider(provider: any): Provider {
-  if (Object.keys(provider).includes('currentProvider')) {
-    return new Web3Provider(provider.currentProvider);
-  } else {
-    return provider;
-  }
-}
+// export function toProvider(provider: any): Provider {
+// 	if (Object.keys(provider).includes('currentProvider')) {
+// 		return new BrowserProvider(provider.currentProvider)
+// 	} else {
+// 		return provider
+// 	}
+// }
 
 export function isJsonFragmentArray(input: any): input is JsonFragment[] {
-  if (!Array.isArray(input)) return false;
-  const inputKeys = Object.keys(input[0]);
-  if (!inputKeys.includes('target') && !inputKeys.includes('function')) return true;
-  return false;
+	if (!Array.isArray(input)) return false
+	const inputKeys = Object.keys(input[0])
+	if (!inputKeys.includes('target') && !inputKeys.includes('function')) return true
+	return false
 }
 
 export function isInterface(input: any): input is Interface {
-  return input._isInterface;
+	return input._isInterface
 }
